a7d9bdfec22d68f64715a2ef7339bda242d16e1c
AlinCristian97 <alin.cristian977@gmail.com>
2021-11-02 10:37:37 +0200

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -12,10 +12,24 @@ import { Product } from '../products';
 export class CartComponent implements OnInit {
   items: Product[] = this.cartService.getItems();
 
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
   constructor(
     private cartService: CartService,
     private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {}
+
+  onSubmit() {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+
+    this.checkoutForm.reset();
+  }
 }
